780779a09e9bf6133c381e671995cdbfec0c13ad
davideC00 <dvdx97@gmail.com>
2020-05-18 19:20:45 +0200

    ADD: Jenkyll site

--- projects/Vibing Cube/sketch.js
+++ projects/Vibing Cube/sketch.js
@@ -5,31 +5,32 @@ let scal;
 let rectHeight;
 let rectWidth;
 let cube;
+let c;
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
   setAttributes('antialias', true);
+  c = createCanvas(800, 400, WEBGL);
+  c.parent('cube');
   angle = PI/2;
   cube = new Cube(100, 20);
 }
 
 function draw() {
   background(150);
-  frameRate(25)
-  
+  frameRate(25);
+
   let locX = mouseX - height / 2;
   let locY = mouseY - width / 2;
-  
+
   stroke(255);
   rotateX(angle += 0.01);
   rotateY(angle += 0.01);
-  
+
   ambientLight(60, 60, 60);
   pointLight(255, 255, 255, locX, locY, 100);
-  
+
   fill(mouseY, 250, mouseX);
 
   cube.setNoise(25);
   cube.createFaces();
-  
 }
